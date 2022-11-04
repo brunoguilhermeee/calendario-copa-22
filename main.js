e4edf082d0828dd921e0f93455b6ab103372657b
bruno santos <brunosantos1234.bs10@gmail.com>
2022-11-04 12:02:10 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,48 @@
+function createGame(player1, hour, player2) {
+  return `
+   <li>
+     <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+     <strong>${hour}</strong>
+     <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+   </li>
+  `
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date} <span>${day}</span></h2>
+
+          <ul>
+           ${games}
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("brazil", "16:00", "servia") +
+      createGame("portgual", "13:00", "gana") +
+      createGame("portgual", "13:00", "gana") +
+      createGame("portgual", "13:00", "gana")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("brazil", "13:00", "suica") +
+      createGame("brazil", "13:00", "suica")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("brazil", "16:00", "camaroes") +
+      createGame("brazil", "16:00", "camaroes") +
+      createGame("brazil", "16:00", "camaroes")
+  )
+
+
